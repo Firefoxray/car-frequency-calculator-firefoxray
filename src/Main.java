d6f594d38ec7be1aad951bad203ffe0a6ae15e9d
Ray Barrett <theboss2136@hotmail.com>
2019-10-02 04:26:25 -0400

    Version 0.1 Concept 2:12 AM 10/2/19 *First Version *System Message Displays *Figuring out what to do *Frame and test message setup
    Version 0.2 Concept
    4:00 AM 10/2/19
    *Added text to background coding
    *Text appears on screen in order
    *Added (Terrible) background visuals
    *Started Debugging Tool
    
    Version 0.23 Concept
    4:15 AM 10/2/19
    *Basic Debugging Tool Complete
    *Resized Window
    *Bug Fixes and Tweaks :^) [Dont ya hate that]

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ Version 0.1 CONCEPT
 **/
 public class Main {
 
-    static double versionNum = 0.2;
+    static double versionNum = 0.23;
     static boolean gdfe = false;//Global Debug Flag Enabler
     public static String welcome = "Car Spawn Frequency Calculator/By Firefoxray, 2019/Version " + versionNum + " STATE: Concept";
     static Calculator calculator = new Calculator();
@@ -15,7 +15,7 @@ public class Main {
         sixLineFix.converter(welcome);
         calculator.calculatorFrame();
 
-        if (gdfe = true) { debug();}
+        //if (gdfe = true) { debug();}
     }
 
     public static void debug(){
